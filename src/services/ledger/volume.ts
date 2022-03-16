255298bacc5aa6dad5698ae0f40d2fe9c249fd9a
vindard <17693119+vindard@users.noreply.github.com>
2022-03-16 16:15:55 -0400

    refactor: swap midPriceFn for PriceRatio in limit checks

--- src/services/ledger/volume.ts
+++ src/services/ledger/volume.ts
@@ -95,7 +95,7 @@ const txVolumeSince = async ({
   }
 }
 
-export const volume: Partial<ILedgerService> = {
+export const volume = {
   allPaymentVolumeSince: volumeFn("allPaymentVolumeSince"),
   externalPaymentVolumeSince: volumeFn("externalPaymentVolumeSince"),
   intraledgerTxBaseVolumeSince: volumeFn("intraledgerTxBaseVolumeSince"),
